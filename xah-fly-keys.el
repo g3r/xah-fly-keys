e633863108b3e635253ae193bd94d3721ae5822a
German Pacenza <germanp1982@gmail.com>
2021-10-30 08:32:09 -0300

    Change mode display color to green.

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -1402,7 +1402,7 @@ Version 2020-04-28"
   (unless no-indication
     (modify-all-frames-parameters '((cursor-type . bar)))
     (set-face-background 'cursor "chartreuse")
-    (setq mode-line-front-space (concat (propertize " INSERT " 'face '(:foreground "burlywood1" :background "royalblue4")) " ")))
+    (setq mode-line-front-space (concat (propertize " INSERT " 'face '(:foreground "burlywood1" :background "#105020")) " ")))
   (force-mode-line-update))
 
 (defun xah-fly-save-buffer-if-file ()
