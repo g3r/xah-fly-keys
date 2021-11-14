c85babf7d65c1630e99f8092877fa9ad435a1219
German Pacenza <germanp1982@gmail.com>
2021-11-13 22:06:16 -0300

    Add space before the lighter.

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -1417,7 +1417,7 @@ Version 2017-07-07"
 URL `http://ergoemacs.org/misc/ergoemacs_vi_mode.html'"
   :group 'xah-fly-keys
   :global t
-  :lighter "XFK"
+  :lighter " XFK"
   :keymap xah-fly-insert-map
   (delete-selection-mode 1)
   (if xah-fly-keys
