a5157bbfb4f854fee85523ec982c7db1d3720b65
German Pacenza <germanp1982@gmail.com>
2022-09-29 22:20:13 -0300

    Revert "Remove space in lighter."
    This reverts commit 26fee8ac58ccb84964204ed24782e9a3d2d78092.
    This space is needed.

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -951,7 +951,7 @@ Version: 2017-07-07"
 URL `http://xahlee.info/emacs/misc/ergoemacs_vi_mode.html'"
   :group 'xah-fly-keys
   :global t
-  :lighter "XFK"
+  :lighter " XFK"
   :keymap xah-fly-insert-map
   (delete-selection-mode 1)
   (setq shift-select-mode nil)
