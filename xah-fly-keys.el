0f1c47eac8c32761041b1e503b566a34a0bb7440
German Pacenza <germanp1982@gmail.com>
2022-10-16 10:59:13 -0300

    Disable lighter.

--- xah-fly-keys.el
+++ xah-fly-keys.el
@@ -787,7 +787,7 @@ Version: 2017-07-07"
 URL `http://xahlee.info/emacs/misc/ergoemacs_vi_mode.html'"
   :group 'xah-fly-keys
   :global t
-  :lighter " XFK"
+  :lighter nil
   :keymap xah-fly-insert-map
   (delete-selection-mode 1)
   (setq shift-select-mode nil)
